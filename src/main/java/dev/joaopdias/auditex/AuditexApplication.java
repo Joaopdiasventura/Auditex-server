9d8266c2d29288ad64fb9abd678ec28b1a9a64e3
João Paulo Dias <joaopdias.dev@gmail.com>
2026-05-07 21:50:01 -0300

    init commit

---
+++ src/main/java/dev/joaopdias/auditex/AuditexApplication.java
@@ -0,0 +1,13 @@
+package dev.joaopdias.auditex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/joaopdias/auditex/AuditexApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joaopdias.auditex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
